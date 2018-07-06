d61eb1160224951bedfcbf70d80b2bb71cae4af9
srsad <artaksergievich@gmail.com>
2018-07-06 17:36:15 +0300

    trigger ajaxForm

--- app/assets/templates/default/js/main.js
+++ app/assets/templates/default/js/main.js
@@ -1,5 +1,16 @@
 $(function() {
 
 	// Custom JS
+	$(document).on('af_complete', function(event, response) {
+		var form = response.form;
+		if (response.success) {
+			$('.closeModal').trigger('click');
+			//var param = {loadpage: 'Отправка формы'};
+			//yaCounter49355807.reachGoal('FORM_GOAL', param);
+			// site order 
+			//gtag('event', 'form_order', { 'event_category': 'form_order', 'event_action': 'sendForm' });
+
+		}
+	});
 
 });
